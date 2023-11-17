81996d778642b67410665a4f8d5f6cfd65fb1935
nimrabaig <nimrabaig28@gmail.com>
2023-11-17 17:19:33 +0500

    added favicon logo

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -5,7 +5,7 @@ import Wrapper from "../layout/wrapper";
 const Home = () => {
   return (
     <Wrapper>
-      <SEO pageTitle={"Technix Main Home"} />
+      <SEO />
       <HomeOne />
     </Wrapper>
   );
