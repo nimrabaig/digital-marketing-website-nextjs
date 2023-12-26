10338deb97efd2529b1bb22a020ab40156024ff3
nimrabaig <nimrabaig28@gmail.com>
2023-12-26 14:59:38 +0500

    remaining files

--- src/common/seo.jsx
+++ src/common/seo.jsx
@@ -8,7 +8,7 @@ const SEO = ({ pageTitle, description }) => (
       </title>
       <meta httpEquiv="x-ua-compatible" content="ie=edge" />
       <meta name="description" content={description} />
-      <meta name="robots" content="noindex, follow" />
+      {/* <meta name="robots" content="noindex, follow" /> */}
       <meta
         name="viewport"
         content="width=device-width, initial-scale=1, shrink-to-fit=no"
